7c684a3921ac064ee5ba0c90ef8749d72d3786b0
jadilson12 <jadilson12@gmail.com>
2018-08-13 16:46:16 -0300

    RR Bringup

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,9 +25,9 @@ VENDOR=motorola
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$CM_ROOT"/vendor/rr/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -38,7 +38,7 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 # Copyright headers and guards
-write_headers
+write_headers "sanders"
 
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
